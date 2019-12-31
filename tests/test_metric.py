fbd77ec9bd472ca672bce6fa16b068ffe39fbcdb
tailaiw <tailai.wen@arundo.com>
2019-12-30 18:57:41 -0600

    added unit test for metrics that divides 0

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+from math import isnan
 from pandas import Timestamp
 from adtk.metrics import recall, precision, iou
 
@@ -13,9 +14,14 @@ s_pred = pd.Series(
     [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, n, 1, 1, n, 0, 1, 0, 1, 1],
     pd.date_range(start=0, periods=24, freq="1d"),
 )
+s0 = pd.Series(
+    [0, 0, 0, 0, 0, 0, 0, n, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, n, 0, 0, 0, 0, 0],
+    pd.date_range(start=0, periods=24, freq="1d"),
+)
 
 df_true = pd.concat([s_true, s_pred], axis=1).rename(columns={0: "A", 1: "B"})
 df_pred = pd.concat([s_pred, s_true], axis=1).rename(columns={0: "A", 1: "B"})
+df0 = pd.concat([s0, s0], axis=1).rename(columns={0: "A", 1: "B"})
 
 
 l_true = [
@@ -35,13 +41,16 @@ l_pred = [
     Timestamp("1970-01-21 00:00:00"),
     (Timestamp("1970-01-23 00:00:00"), Timestamp("1970-01-24 00:00:00")),
 ]
+l0 = []
 
 d_true = {"A": l_true, "B": l_pred}
 d_pred = {"A": l_pred, "B": l_true}
+d0 = {"A": l0, "B": l0}
 
 
 def test_metric_series():
     assert recall(s_true, s_pred) == 9 / 12
+    assert isnan(recall(s0, s_pred))
     assert precision(s_true, s_pred) == 9 / 15
     assert iou(s_true, s_pred) == 9 / 17
     assert iou(s_pred, s_true) == 9 / 17
@@ -49,6 +58,7 @@ def test_metric_series():
 
 def test_metric_list():
     assert recall(l_true, l_pred) == 4 / 6
+    assert isnan(recall(l0, l_pred))
     assert precision(l_true, l_pred) == 4 / 7
     assert recall(l_true, l_pred, thresh=1) == 3 / 6
     assert precision(l_true, l_pred, thresh=1) == 3 / 7
@@ -57,12 +67,18 @@ def test_metric_list():
 
 def test_metric_dataframe():
     assert recall(df_true, df_pred) == {"A": 9 / 12, "B": 9 / 15}
+    assert all([isnan(x) for x in recall(df0, df_pred).values()]) and (
+        recall(df0, df_pred).keys() == {"A": n, "B": n}.keys()
+    )
     assert precision(df_true, df_pred) == {"A": 9 / 15, "B": 9 / 12}
     assert iou(df_true, df_pred) == {"A": 9 / 17, "B": 9 / 17}
 
 
 def test_metric_dict():
     assert recall(d_true, d_pred) == {"A": 4 / 6, "B": 4 / 7}
+    assert all([isnan(x) for x in recall(d0, d_pred).values()]) and (
+        recall(d0, d_pred).keys() == {"A": n, "B": n}.keys()
+    )
     assert precision(d_true, d_pred) == {"A": 4 / 7, "B": 4 / 6}
     assert recall(d_true, d_pred, thresh=1) == {"A": 3 / 6, "B": 3 / 7}
     assert precision(d_true, d_pred, thresh=1) == {"A": 3 / 7, "B": 3 / 6}
