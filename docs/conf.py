arundo/adtk

commit 119a5a971d7b3b1fb433a92faff325d786aef31b

optimize index check to time dependent transformers (#90)
* optimize index check to time dependent transformers

* updated changelog

* updated version number

docs/conf.py
@@ -70,7 +70,7 @@ author = "Arundo Analytics, Inc"
 # The short X.Y version.
 version = "0.6"
 # The full version, including alpha/beta/rc tags.
-release = "0.6.0-dev.23+pr.89"
+release = "0.6.0-dev.24+pr.90"
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

setup.cfg
@@ -1,6 +1,6 @@
 [metadata]
 name = adtk
-version = 0.6.0-dev.23+pr.89
+version = 0.6.0-dev.24+pr.90
 author = Arundo Analytics, Inc.
 maintainer = Tailai Wen
 maintainer_email = tailai.wen@arundo.com

src/adtk/__init__.py
@@ -20,4 +20,4 @@ See https://arundo-adtk.readthedocs-hosted.com for complete documentation.
 
 """
 
-__version__ = "0.6.0-dev.23+pr.89"
+__version__ = "0.6.0-dev.24+pr.90"

src/adtk/transformer/_transformer_1d.py
@@ -6,7 +6,6 @@ original time series.
 
 """
 
-import warnings
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
@@ -217,11 +216,7 @@ class RollingAggregate(_NonTrainableUnivariateTransformer):
         if not (
             s.index.is_monotonic_increasing or s.index.is_monotonic_decreasing
         ):
-            warnings.warn(
-                "Time series does not have a monotonic increasing time index. "
-                "Results from this model may be unreliable.",
-                UserWarning,
-            )
+            raise ValueError("Time series must have a monotonic time index. ")
 
         agg = self.agg
         agg_params = self.agg_params if (self.agg_params is not None) else {}
@@ -484,11 +479,7 @@ class DoubleRollingAggregate(_NonTrainableUnivariateTransformer):
         if not (
             s.index.is_monotonic_increasing or s.index.is_monotonic_decreasing
         ):
-            warnings.warn(
-                "Time series does not have a monotonic increasing time index. "
-                "Results from this model may be unreliable.",
-                UserWarning,
-            )
+            raise ValueError("Time series must have a monotonic time index. ")
 
         agg = self.agg
         agg_params = self.agg_params if (self.agg_params is not None) else {}
@@ -692,11 +683,7 @@ class ClassicSeasonalDecomposition(_TrainableUnivariateTransformer):
         if not (
             s.index.is_monotonic_increasing or s.index.is_monotonic_decreasing
         ):
-            warnings.warn(
-                "Time series does not have a monotonic increasing time index. "
-                "Results from this model may be unreliable.",
-                UserWarning,
-            )
+            raise ValueError("Time series must have a monotonic time index. ")
         # remove starting and ending nans
         s = s.loc[s.first_valid_index() : s[::-1].first_valid_index()].copy()
         if pd.isna(s).any():
@@ -749,11 +736,7 @@ class ClassicSeasonalDecomposition(_TrainableUnivariateTransformer):
         if not (
             s.index.is_monotonic_increasing or s.index.is_monotonic_decreasing
         ):
-            warnings.warn(
-                "Time series does not have a monotonic increasing time index. "
-                "Results from this model may be unreliable.",
-                UserWarning,
-            )
+            raise ValueError("Time series must have a monotonic time index. ")
         # check if series freq is same
         if self._series_freq not in {s.index.freqstr, s.index.inferred_freq}:
             raise RuntimeError(
@@ -963,10 +946,11 @@ class Retrospect(_NonTrainableUnivariateTransformer):
         if not (
             s.index.is_monotonic_increasing or s.index.is_monotonic_decreasing
         ):
-            warnings.warn(
-                "Time series does not have a monotonic increasing time index. "
-                "Results from this model may be unreliable.",
-                UserWarning,
+            raise ValueError("Time series must have a monotonic time index. ")
+        if (s.index.freq is None) and (s.index.inferred_freq):
+            raise RuntimeError(
+                "Series does not follow any known frequency "
+                "(e.g. second, minute, hour, day, week, month, year, etc."
             )
         n_steps = self.n_steps
         till = self.till

commit 615ae8ab65a94de3da98f9d1d728042729e31b7e

Fixed a bug introduced by #90 (#91)
* fixed a bug

* updated version

docs/conf.py
@@ -70,7 +70,7 @@ author = "Arundo Analytics, Inc"
 # The short X.Y version.
 version = "0.6"
 # The full version, including alpha/beta/rc tags.
-release = "0.6.0-dev.24+pr.90"
+release = "0.6.0-dev.25+pr.91"
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.

setup.cfg
@@ -1,6 +1,6 @@
 [metadata]
 name = adtk
-version = 0.6.0-dev.24+pr.90
+version = 0.6.0-dev.25+pr.91
 author = Arundo Analytics, Inc.
 maintainer = Tailai Wen
 maintainer_email = tailai.wen@arundo.com

src/adtk/__init__.py
@@ -20,4 +20,4 @@ See https://arundo-adtk.readthedocs-hosted.com for complete documentation.
 
 """
 
-__version__ = "0.6.0-dev.24+pr.90"
+__version__ = "0.6.0-dev.25+pr.91"

src/adtk/_base.py
@@ -300,7 +300,9 @@ class _TrainableMultivariateModel(_TrainableModel):
                         self._cols, list(set(self._cols) - set(df.columns))
                     )
                 )
-            df_copy = df.loc[:, self._cols].copy()
+            df_copy = (
+                df.loc[:, self._cols].copy() if self._cols else df.copy()
+            )  # in a customized hd model that doesn't need fit, self._cols is empty
             predicted = self._predict_core(df_copy)
         else:
             raise TypeError("Input must be a pandas DataFrame.")